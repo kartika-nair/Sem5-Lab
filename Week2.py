ebee624f96c0abade58cc7bd9affd703c077cbf5
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-09-03 15:06:12 +0530

    many errors

--- Week2.py
+++ Week2.py
@@ -1,9 +1,25 @@
+def helperFunc(visited, cost, start_point, goals, path):
+
+    path += [start_point]
+    visited[start_point] = 1
+
+    if start_point in goals:
+        return path
+    
+    costList = enumerate(cost[start_point])
+
+    for curr, length in costList:
+        if visited[curr] or not curr or length <= 0:
+            continue
+        helperFunc(cost, curr, goals, visited, path)
+        return path
+
+    return path
+
 """
 You can create any other helper funtions.
 Do not modify the given functions
 """
-
-import queue
 
 def A_star_Traversal(cost, heuristic, start_point, goals):
     """
@@ -20,29 +36,32 @@ def A_star_Traversal(cost, heuristic, start_point, goals):
     path = []
     
     # TODO
-    priority = queue.PriorityQueue()
-    priority.put((heuristic[start_point], ([start_point], start_point, 0)))
+    
+    queue = [[start_point, 0]]
 
-    while(priority.qsize()):
-        cost2, nodes = priority.get()
-        path = nodes[0]
-        curr = nodes[1]
-        nodeCost = nodes[2]
+    visited = [[0, -1, None] for i in cost]
+    g = [0 for i in cost]
 
-    n = len(cost)
-    visited = [0 for i in range(n)]
-        
-    if visited[curr] == 0:
-        visited[curr] = 1
-    if curr in goals:
-        return path
+    while len(queue) != 0:
+        node, f = queue.pop(0)
 
-    for next in range(1, n):
-        if cost[curr][next] > 0 and visited[next] == 0:
-            total = nodeCost + cost[curr][next]
-            cost2 = total + heuristic[next]
-            path.append(next)
-            priority.put((cost2, (path, next, total)))
+        if node in goals:
+            print(node)
+            return
+
+        for neighbour, dist in enumerate(cost[node]):
+            if dist <= 0:
+                continue
+
+            g[neighbour] = g[node] + dist
+            f = g[neighbour] + heuristic[neighbour]
+
+            if not visited[neighbour][0] or visited[neighbour][1] > f:
+                visited[neighbour] = [1, f, node]
+                # insert into priority queue/min heap
+                
+
+        path = sorted(queue, key=lambda x: x[1])[0]
 
     # DONE
     
@@ -64,27 +83,31 @@ def DFS_Traversal(cost, start_point, goals):
     
     # TODO
     
-    stack = queue.LifoQueue(maxsize = 0)
-        
-    n = len(cost)
-    visited = [0 for i in range(n)]
-
-    stack.put((start_point, [start_point]))
-
-    while(stack.qsize()):
-        node, path = stack.get()
-    if visited[node] == 0:
-        visited[node] = 1
-    if node in goals:
-        return path
-                
-    else:
-        for next in range(n-1, 0, -1):
-            if cost[node][next] > 0:
-                if visited[next] == 0:
-                    path.append(next)
-                    stack.put((next, path))
+    visited = [0 for i in cost]
+    path = helperFunc(visited, cost, start_point, goals, path)
     
     # DONE
     
     return path
+    
+    
+
+
+'''
+cost = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 5, 9, -1, 6, -1, -1, -1, -1, -1],
+            [0, -1, 0, 3, -1, -1, 9, -1, -1, -1, -1],
+            [0, -1, 2, 0, 1, -1, -1, -1, -1, -1, -1],
+            [0, 6, -1, -1, 0, -1, -1, 5, 7, -1, -1],
+            [0, -1, -1, -1, 2, 0, -1, -1, -1, 2, -1],
+            [0, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1],
+            [0, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1],
+            [0, -1, -1, -1, -1, 2, -1, -1, 0, -1, 8],
+            [0, -1, -1, -1, -1, -1, -1, -1, -1, 0, 7],
+            [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]]
+heuristic = [0, 5, 7, 3, 4, 6, 0, 0, 6, 5, 0]
+start = 1
+goals = [6, 7, 10]
+
+print(DFS_Traversal(cost,start, goals))
+'''
