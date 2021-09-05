f5ad5211dedf76af5c903591a921b724fc528b09
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-09-05 18:40:33 +0530

    removed unnecessary comments

--- Week2.py
+++ Week2.py
@@ -6,6 +6,7 @@ Do not modify the given functions
 import sys
 import queue
 
+
 def A_star_helper(priority, visited, goals, path, start_point, parents, cost, heuristic, minimum_cost):
     '''
     HELPER FUNCTION FOR A STAR
@@ -70,36 +71,6 @@ def A_star_Traversal(cost, heuristic, start_point, goals):
     parents = {start_point: start_point}
     visited = set()
 
-    # while(priority.qsize()):
-    #     t = priority.get()
-    #     node = t[1]
-    #     if node in visited:
-    #         continue
-
-    #     if node in goals:
-    #         path = [start_point]
-    #         if start_point == node:
-    #             return path
-    #         path = [node]
-    #         node1 = node
-    #         while not node1 == start_point:
-    #             node1 = parents[node1]
-    #             path.insert(0, node1)
-    #         return path
-
-    #     visited.add(node)
-
-    #     for child in range(1, len(cost[node])):
-    #         if cost[node][child] in [0, -1] or child in visited:
-    #             continue
-
-    #         temp_total = cost[node][child] + minimum_cost[node]
-    #         if minimum_cost[child] == infinity or temp_total <= minimum_cost[child]:
-    #             minimum_cost[child] = temp_total
-    #             parents[child] = node
-
-    #         priority.put((minimum_cost[child] + (0 if child >= len(heuristic) else heuristic[child]), child))
-
     path = A_star_helper(priority, visited, goals, path, start_point, parents, cost, heuristic, minimum_cost)
 
     # DONE
@@ -158,30 +129,6 @@ def DFS_Traversal(cost, start_point, goals):
     visited = [0 for i in range(n)]
     parents = {}
     stack.put((start_point, [start_point]))
-
-    # while(stack.qsize()):
-    #     node, path = stack.get()
-
-    #     if visited[node] == 0:
-    #         visited[node] = 1
-    #     else:
-    #         continue
-    #     if node in goals:
-    #         path = [node]
-    #         if start_point == node:
-    #             return path
-    #         cur = node
-    #         while not cur == start_point:
-    #             cur = parents[cur]
-    #             path.insert(0, cur)
-    #         return path
-
-    #     else:
-    #         for i in range(len(cost[node]) - 1, 0, -1):
-    #             if visited[i] or cost[node][i] in [0, -1]:
-    #                 continue
-    #             parents[i] = node
-    #             stack.put((i, path))
 
     path = DFS_helper(stack, path, visited, goals, start_point, parents, cost)
 
