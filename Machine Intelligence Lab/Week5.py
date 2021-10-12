c1da357c0fb9b05ae4a2eebd7448b274feb69e25
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-10-13 01:10:03 +0530

    mi week5 (some parts in french fancy)

--- Machine Intelligence Lab/Week5.py
+++ Machine Intelligence Lab/Week5.py
@@ -109,6 +109,20 @@ class Tensor:
 
         return out_tensor
 
+    def grad_add_helper(self, gradients=None):
+        if(self.history[1].requires_grad):
+            for i in range(self.arr.shape[0]):
+                for j in range(self.arr.shape[1]):
+                    self.history[1].grad[i][j] += self.history[1].arr[i][j] / self.history[1].arr[i][j]
+        
+        if(self.history[2].requires_grad):
+            for i in range(self.arr.shape[0]):
+                for j in range(self.arr.shape[1]):
+                    self.history[2].grad[i][j] += self.history[2].arr[i][j] / self.history[2].arr[i][j]
+
+        return((self.history[1].grad, self.history[2].grad))
+
+
     def grad_add(self, gradients=None):
         """
         Find gradients through add operation
@@ -122,7 +136,21 @@ class Tensor:
                 Gradient to a and b
         """
         # TODO
-        pass
+
+        return(gradients, gradients)
+
+        # DONE
+        # pass
+
+    def degrades(self, gradients=None):
+        degrades_une_213 = np.ones_like(self.history[1].arr)
+        degrades_deux_213 = np.ones_like(self.history[2].arr)
+
+        degrades_une_213 = gradients @ np.transpose(self.history[2].arr)
+        degrades_deux_213 = np.transpose(self.history[1].arr) @ gradients
+
+        return(degrades_une_213, degrades_deux_213)
+
 
     def grad_matmul(self, gradients=None):
         """
@@ -137,7 +165,11 @@ class Tensor:
                 Gradients to a and b
         """
         # TODO
-        pass
+
+        return((self.degrades(gradients)))
+
+        # DONE
+        # pass
 
     def backward(self, gradients=None):
         """
@@ -158,4 +190,115 @@ class Tensor:
             Nothing. (The gradients of leaf have to set in their respective attribute(leafobj.grad))
         """
         # TODO
-        pass
+
+        try:
+            if(gradients == None):
+                gradients = np.ones_like(self.arr)
+        except:
+            pass
+
+        if(self.history[0] == 'leaf'):
+            if(self.requires_grad == True):
+                self.grad += gradients
+
+        elif(self.history[0] == 'add'):
+            self.history[1].backward(self.grad_add(gradients)[0])
+            self.history[2].backward(self.grad_add(gradients)[1])
+
+        elif(self.history[0] == 'matmul'):
+            self.history[1].backward(self.grad_matmul(gradients)[0])
+            self.history[2].backward(self.grad_matmul(gradients)[1])
+
+        # DONE
+        # pass
+        
+
+'''
+a = Tensor(np.array([[1.0, 2.0], [3.0, 4.0]]))
+b = Tensor(np.array([[3.0, 2.0], [1.0, 5.0]]), requires_grad=False)
+c = Tensor(np.array([[3.2, 4.5], [6.1, 4.2]]))
+z = np.array([[0.0, 0.0], [0.0, 0.0]])
+sans = a+b
+sans2 = a+a
+mulans = a@b
+mulans2 = (a+b)@c
+sgrad = np.array([[1.0, 1.0], [1.0, 1.0]])
+sgrad2 = np.array([[2.0, 2.0], [2.0, 2.0]])
+mulgrad = np.array([[5.0, 6.0], [5.0, 6.0]])
+mulgrad2 = np.array([[4.0, 4.0], [6.0, 6.0]])
+mulgrad3 = np.array([[7.7, 10.29], [7.7, 10.29]])
+mulgrad4 = np.array([[8.0, 8.0], [13.0, 13.0]])
+
+
+def test_case():
+
+    try:
+        sans.backward()
+        np.testing.assert_array_almost_equal(a.grad, sgrad, decimal=2)
+        print("Test Case 1 for the function Add Grad PASSED")
+    except:
+        print("Test Case 1 for the function Add Grad FAILED")
+
+    try:
+        np.testing.assert_array_almost_equal(b.grad, z, decimal=2)
+        print("Test Case 2 for the function Add Grad PASSED")
+    except:
+        print("Test Case 2 for the function Add Grad FAILED")
+
+    a.zero_grad()
+    b.zero_grad()
+
+    try:
+        sans2.backward()
+        np.testing.assert_array_almost_equal(a.grad, sgrad2, decimal=2)
+        print("Test Case 3 for the function Add Grad PASSED")
+    except:
+        print("Test Case 3 for the function Add Grad FAILED")
+
+    a.zero_grad()
+    b.zero_grad()
+
+    try:
+        mulans.backward()
+        np.testing.assert_array_almost_equal(a.grad, mulgrad, decimal=2)
+        print("Test Case 4 for the function Matmul Grad PASSED")
+    except:
+        print("Test Case 4 for the function Matmul Grad FAILED")
+
+    try:
+        np.testing.assert_array_almost_equal(b.grad, z, decimal=2)
+        print("Test Case 5 for the function Matmul Grad PASSED")
+    except:
+        print("Test Case 5 for the function Matmul Grad FAILED")
+
+    a.zero_grad()
+    b.zero_grad()
+    b.requires_grad = True
+
+    try:
+        mulans.backward()
+        np.testing.assert_array_almost_equal(b.grad, mulgrad2, decimal=2)
+        print("Test Case 6 for the function Matmul Grad PASSED")
+    except:
+        print("Test Case 6 for the function Matmul Grad FAILED")
+
+    a.zero_grad()
+    b.zero_grad()
+    c.zero_grad()
+
+    try:
+        mulans2.backward()
+        np.testing.assert_array_almost_equal(a.grad, mulgrad3, decimal=2)
+        np.testing.assert_array_almost_equal(b.grad, mulgrad3, decimal=2)
+        print("Test Case 7 for the function Matmul and add Grad PASSED")
+    except:
+        print("Test Case 7 for the function Matmul and add Grad FAILED")
+
+    try:
+        np.testing.assert_array_almost_equal(c.grad, mulgrad4, decimal=2)
+        print("Test Case 8 for the function Matmul and add Grad PASSED")
+    except:
+        print("Test Case 8 for the function Matmul and add Grad FAILED")
+
+test_case()
+'''
