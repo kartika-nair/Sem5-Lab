ff35d8080ab808dd365c69ad46ab2fdebd20e4bc
Kartika <56603537+kartika-nair@users.noreply.github.com>
2021-11-12 14:34:59 +0530

    final mi w8

--- Machine Intelligence Lab/Week8.py
+++ Machine Intelligence Lab/Week8.py
@@ -27,16 +27,26 @@ class HMM:
         """
         self.states_dict = dict(zip(self.states, list(range(self.N))))
         self.emissions_dict = dict(zip(self.emissions, list(range(self.M))))
-
-    def initialisation_213(self, i):
-        pass
-
-    def termination_213(self, i):
-        pass
-
-    def recursion_213(self, i):
-        pass
-
+    
+    
+    def init_213 (self, t):
+        return self.pi[t]
+    
+    def main_init_213 (self, s, t, seq):
+        return self.pi[t] * self.B[self.states_dict[s[t]]][self.emissions_dict[seq[t]]]
+    
+    def rec_213 (self, s, t, seq):
+        if (t == 1):
+            return self.init_213(0) * self.B[self.states_dict[s[0]]][self.emissions_dict[seq[0]]] * self.A[self.states_dict[s[t]]][self.states_dict[s[t-1]]] * self.B[self.states_dict[s[t]]][self.emissions_dict[seq[t]]]
+        
+        else:
+            return self.rec_213(s, t-1, seq) * self.A[self.states_dict[s[t]]][self.states_dict[s[t-1]]] * self.B[self.states_dict[s[t]]][self.emissions_dict[seq[t]]]
+    
+    def product_213(self, *args, reps):
+        result = [[]]
+        for i in [tuple(i) for i in args] * reps: result = [x+[y] for x in result for y in i]
+        for prod in result: yield tuple(prod)
+    
     def viterbi_algorithm(self, seq):
         """
         Function implementing the Viterbi algorithm
@@ -48,16 +58,19 @@ class HMM:
         """
         # TODO
         
-        hidden_states_sequence_213 = np.zeros(len(seq) + 1)
+        hidden_states_sequence_213 = []
+        nu_213 = 0
 
-        for i in len(seq):
-            if (i == 1):
-                hidden_states_sequence_213[i] = initialisation_213(i)
-            elif (i == len(seq)):
-                hidden_states_sequence_213[i] = termination_213(i)
-            else:
-                hidden_states_sequence_213[i] = recursion_213(i)
+        for s in self.product_213(self.states, reps = len(seq)):
+            maybe_nu_213 = 0
+            for t in range(len(seq)):
+                if (t == 0):
+                    maybe_nu_213 = self.main_init_213(list(s), t, seq)
+                else:
+                    maybe_nu_213 = self.rec_213(list(s), t, seq)
+            if(maybe_nu_213 > nu_213):
+                hidden_states_sequence_213 = list(s)
+                nu_213 = maybe_nu_213
 
-        return hidden_states_sequence_213[1:]
-
+        return hidden_states_sequence_213
         # DONE
